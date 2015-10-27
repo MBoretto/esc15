a7b9084f07ab7b1c5597765473932f1d07a7cbdc
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-10-27 07:28:47 +0100

    faster count

--- hands-on/floatingpoint/count.cpp
+++ hands-on/floatingpoint/count.cpp
@@ -3,7 +3,7 @@
 #include<cmath>
 #include<limits>
 #include<cstdio>
-
+#include<cstring>
 
 int count(float x, float e) {
   int c=0;
@@ -11,11 +11,18 @@ int count(float x, float e) {
   return c;
 }
 
+int diff(float x, float e) {
+  int ix=0; memcpy(&ix,&x,sizeof(int));
+  int ie=0; memcpy(&ie,&e,sizeof(int));
+  return ie-ix;
+}
 int main() {
 
 
   std::cout << count(.1f,1.f) << std::endl;
   std::cout << count(2.f,3.f) << std::endl;
+  std::cout << diff(.1f,1.f) << std::endl;
+  std::cout << diff(2.f,3.f) << std::endl;
 		     
   return 0;
 
